3c19bf35f0b67c552449b5eede54f4dfe00dfae4
Cole Smith <cole.s.smith1@gmail.com>
2015-02-03 13:00:06 -0500

    Initial Commit

---
+++ Shorecrest Web Browser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shorecrest Web Browser
+//
+//  Created by Cole Smith on 2/3/15.
+//  Copyright (c) 2015 Shorecrest Preparatory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
