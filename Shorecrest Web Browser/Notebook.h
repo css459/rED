a9caa70c4eb8586a2b991810200c02b3e362d468
Cole Smith <cole.s.smith1@gmail.com>
2015-02-04 14:06:03 -0500

    Major changes to class file structure and more Storyboard improvements

---
+++ Shorecrest Web Browser/Notebook.h
@@ -0,0 +1,26 @@
+//
+//  Notebook.h
+//  Shorecrest Web Browser
+//
+//  Created by Cole Smith on 2/4/15.
+//  Copyright (c) 2015 Shorecrest Preparatory. All rights reserved.
+//
+
+/*
+    The Note object stores the notes data that the user creates
+    Every "Page" object should have a Note object. 
+ 
+    Notes, when displayed in the Notes View Controller should follow this convention:
+ 
+        "Vitae class luctus convallis vestibulum." - Line 42
+ 
+            Here, the user will write a note about the quoted line above from the text
+            The note should be indented as shown and have margins left aligned.
+            Notes should be separated by a generous about of space or a thin line.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface Notebook : NSObject
+
+@end
--- Shorecrest Web Browser/Notebook.m
+++ Shorecrest Web Browser/Notebook.m
@@ -1,13 +1,13 @@
 //
-//  Note.h
+//  Notebook.m
 //  Shorecrest Web Browser
 //
 //  Created by Cole Smith on 2/4/15.
 //  Copyright (c) 2015 Shorecrest Preparatory. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "Notebook.h"
 
-@interface Note : NSObject
+@implementation Notebook
 
 @end
