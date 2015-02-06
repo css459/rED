6739f10328793650f70a3ba7b4def659cce5067d
Cole Smith <cole.s.smith1@gmail.com>
2015-02-06 09:16:09 -0500

    settings updates file management

---
+++ rED/Settings.h
@@ -0,0 +1,18 @@
+//
+//  Settings.h
+//  rED
+//
+//  Created by Cole Smith on 2/6/15.
+//  Copyright (c) 2015 Shorecrest Preparatory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Settings : NSObject
+
+@property BOOL nightMode;
+@property BOOL tutorialMode;
+@property int textSize;
+@property NSString *homeStie;
+
+@end
---
+++ rED/Settings.m
@@ -0,0 +1,29 @@
+//
+//  Settings.m
+//  rED
+//
+//  Created by Cole Smith on 2/6/15.
+//  Copyright (c) 2015 Shorecrest Preparatory. All rights reserved.
+//
+
+#import "Settings.h"
+
+@implementation Settings
+
++ (Settings *)sharedSettings {
+    static Settings *sharedSettings = nil;
+    if (!sharedSettings) {
+        sharedSettings = [[super allocWithZone:nil] init];
+    }
+    return sharedSettings;
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
+
+
+@end
