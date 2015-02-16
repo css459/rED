02961ebc0dd7d1fbc5f49faed954261dd145f681
Cole Smith <cole.s.smith1@gmail.com>
2015-02-16 15:38:48 -0500

    Notes added

--- rED/AutoScrollViewController.h
+++ rED/AutoScrollViewController.h
@@ -6,6 +6,17 @@
 //  Copyright (c) 2015 Shorecrest Preparatory. All rights reserved.
 //
 
+/*
+ The Auto-Scroll behavior should be as follows:
+    The Pan Gesture Recognizer will change the scrolling speed
+        UP for slower
+        DOWN for Faster
+    The Tap Gesture Recognizer will pause / play the scrolling
+        DOUBLE TAP will stop scrolling
+        Stopping the scroll will allow the user to scroll manually as usual
+        DOUBLE TAP to start auto scroll again
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface AutoScrollViewController : UIViewController
