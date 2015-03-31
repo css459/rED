94daa83e226396657f9fd2a3d91e1cd7dac0216c
Cole Smith <cole.s.smith1@gmail.com>
2015-03-31 11:55:58 -0400

    Autoscroll pull-over

--- rED/AutoScrollViewController.h
+++ rED/AutoScrollViewController.h
@@ -8,24 +8,56 @@
 
 /*
  The Auto-Scroll behavior should be as follows:
-    The Pan Gesture Recognizer will change the scrolling speed
-        UP for slower
-        DOWN for Faster
-    The Tap Gesture Recognizer will pause / play the scrolling
-        DOUBLE TAP will stop scrolling
-        Stopping the scroll will allow the user to scroll manually as usual
-        DOUBLE TAP to start auto scroll again
+ The Pan Gesture Recognizer will change the scrolling speed
+ UP for slower
+ DOWN for Faster
+ The Tap Gesture Recognizer will pause / play the scrolling
+ DOUBLE TAP will stop scrolling
+ Stopping the scroll will allow the user to scroll manually as usual
+ DOUBLE TAP to start auto scroll again
  */
 
 #import <UIKit/UIKit.h>
-
+//#import <UIGestureRecognizerDelegate>
 @interface AutoScrollViewController : UIViewController
 
+
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property float scrollVal;
+
+@property (nonatomic, strong)NSTimer *timer;
+
+@property int scrollSpeed;
+
+@property (weak, nonatomic) IBOutlet UIToolbar *stopButton;
+@property (weak, nonatomic) IBOutlet UIToolbar *fastButton;
+@property (weak, nonatomic) IBOutlet UIToolbar *slowButton;
+@property (weak, nonatomic) IBOutlet UIToolbar *scrollButton;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *button_Done;
 @property (weak, nonatomic) IBOutlet UIStepper *stepper_ScrollSpeed;
+
+@property int h;
 
 - (IBAction)button_DoneWasPressed:(id)sender;
 - (IBAction)stepper_ScrollSpeedDidChange:(id)sender;
 
 
+-(void)scroll;
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event inView:(UIView *)view;
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event inView:(UIView *)view;
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event inView:(UIView *)view;
+
+-(void)didSwipe:(UISwipeGestureRecognizer*)swipe;
+
 @end
+
+
+
+//UIButtonHidden
+//  Depending on swiping up or down to change speed
+//      Popup tells you what level speed then quickly disappears
--- rED/AutoScrollViewController.m
+++ rED/AutoScrollViewController.m
@@ -10,11 +10,11 @@
  NOTE:
  
  *The behavior of scroll speed dialing has changed.
-    Instead, we will use a gesture of scrolling thumb up in vc to increase
-    scroll speed and down to decrease scroll speed.
+ Instead, we will use a gesture of scrolling thumb up in vc to increase
+ scroll speed and down to decrease scroll speed.
  
-    - This clears up the view and allows the device to be held more naturally without
-    reaching for the toolbar
+ - This clears up the view and allows the device to be held more naturally without
+ reaching for the toolbar
  
  */
 
@@ -25,17 +25,21 @@
 @end
 
 @implementation AutoScrollViewController
-@synthesize stepper_ScrollSpeed;
+
+@synthesize stepper_ScrollSpeed, scrollVal, scrollButton, scrollSpeed, scrollView, h, timer, webView;
 
 #pragma mark - Initializers
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        //stepper_ScrollSpeed.maximumValue = ;
-        //stepper_ScrollSpeed.minimumValue = ;
-        //stepper_ScrollSpeed.value = ;
+        //        stepper_ScrollSpeed.maximumValue = 5.0;
+        //        stepper_ScrollSpeed.minimumValue = 0.0;
+        //        [stepper_ScrollSpeed setValue:5.0 ];
+        //            NSLog(@"before button pressed:%ld",(long)[stepper_ScrollSpeed value]);
     }
+    h = 0;
+    scrollVal = 0.0f;
     return self;
 }
 
@@ -43,7 +47,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    stepper_ScrollSpeed.maximumValue = 5.0;
+    stepper_ScrollSpeed.minimumValue = 0.0;
+    [stepper_ScrollSpeed setValue:0.05];
+    NSLog(@"before button pressed:%ld",(long)[stepper_ScrollSpeed value]);
+    
+    NSString *urlAddress = @"http://www.reddit.com/";
+    NSURL *url = [NSURL URLWithString:urlAddress];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:requestObj];
+    
     [self.navigationController setToolbarHidden:YES];
+    
+}
+
+
+-(IBAction)stopButtonWasPressed:(id)sender
+{
+    
+    [timer invalidate];
+    timer = nil;
+    
+}
+
+//This will be called every second
+-(void)scroll
+{
+    
+    CGPoint bottomOffset = CGPointMake(0, self.webView.scrollView.contentSize.height - self.webView.scrollView.bounds.size.height);
+    if (scrollVal < bottomOffset.y) {
+        CGPoint scroll = CGPointMake(0, scrollVal);
+        scrollVal += 1;
+        
+        [self.webView.scrollView setContentOffset:scroll animated:YES];
+        webView.scrollView.bounces = NO;
+        
+    } else {
+        [timer invalidate];
+        timer = nil;
+    }
+    
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    [timer invalidate];
+    timer = nil;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -60,16 +112,28 @@
 }
 
 // Adjust the scroll speed
-- (IBAction)stepper_ScrollSpeedDidChange:(id)sender {
+- (IBAction)stepper_ScrollSpeedDidChange:(UIStepper *)sender {
+    
+    NSInteger value = .05*stepper_ScrollSpeed.value + .05;
+    
+    [timer invalidate];
+    timer = nil;
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:value target:self selector:@selector(scroll) userInfo:nil repeats:YES];
+    
+    NSLog(@"button pressed:%ld",(long)stepper_ScrollSpeed.value);
+    
 }
+
+
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 @end
