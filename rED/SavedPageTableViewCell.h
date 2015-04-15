ac378890ac718af944ab9d313d2af1d68401052b
Cole Smith <cole.s.smith1@gmail.com>
2015-04-15 13:33:09 -0400

    Added support for saved pages view controller

---
+++ rED/SavedPageTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  SavedPageTableViewCell.h
+//  rED
+//
+//  Created by Cole Smith on 4/15/15.
+//  Copyright (c) 2015 Shorecrest Preparatory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SavedPageTableViewCell : UITableViewCell
+
+// Cell Properties
+@property (weak, nonatomic) IBOutlet UILabel *label_savePage;
+@property (weak, nonatomic) IBOutlet UILabel *label_dateAdded;
+@property (weak, nonatomic) IBOutlet UITextView *textView_pagePreview;
+
+@end
---
+++ rED/SavedPageTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SavedPageTableViewCell.m
+//  rED
+//
+//  Created by Cole Smith on 4/15/15.
+//  Copyright (c) 2015 Shorecrest Preparatory. All rights reserved.
+//
+
+#import "SavedPageTableViewCell.h"
+
+@implementation SavedPageTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
